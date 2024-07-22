97e315b6e5558f72eed7dec28a6457142a505fd1
pfoisse <49710916+pfoisse@users.noreply.github.com>
2024-07-22 14:20:47 +0200

    ajout de la fonction getdirectory name

--- files/pfo_files.py
+++ files/pfo_files.py
@@ -2,7 +2,8 @@
 
 import os
 
-def list_files_in_repertoire(folder_path):
+# ! --------------------------------------------------------------------------------------
+def list_files_in_directory(folder_path):
     """
     Lister le contenu d un répertoire
 
@@ -22,3 +23,73 @@ def list_files_in_repertoire(folder_path):
                     and not f.startswith('.')])
 
     return fichiers
+
+# ! --------------------------------------------------------------------------------------
+def list_all_files(folder_path):
+    """
+    Lister le contenu d un répertoire y compris dans les sous-répertoires
+
+    Args:
+        folder_path (chain): chemin vers le répertoire
+        ex : folder_path = '/Users/pierre/Desktop/images'
+
+    Returns:
+        liste avec le contenu du répertoire et des sous-répertoires
+
+    Example:
+        
+    """
+    # Initialiser la liste
+    liste = []
+
+    # initialiser le chemin
+    rootdir = folder_path
+
+    # Lister les sous-répertoires
+    for subdir, _, files in os.walk(rootdir):
+        # Lister les fichiers dans le répertoire
+        for file in files:
+            # Ajouter le chemin complet du fichier à la liste
+            a = os.path.join(subdir, file)
+            # Ajouter le chemin à la liste
+            liste.append(a)
+    
+    liste = sorted(liste)
+
+    return liste
+
+# ! --------------------------------------------------------------------------------------
+def get_directory_name(item):
+    """
+    Donner le nom du répertoire qui contient un fichier
+    Dans ce code, la fonction get_directory_name utilise la fonction os.path.dirname
+    pour obtenir le nom du répertoire qui contient le fichier. 
+    Ensuite, elle utilise la méthode split pour séparer le nom du répertoire 
+    de la chaîne de chemin d'accès, et elle retourne le dernier élément 
+    de la liste, qui est le nom du répertoire.
+
+    Args:
+        file (chain): nom du fichier
+        ex : ''
+
+    Returns:
+        (chain) nom du répertoire 
+
+    Example:
+        
+    """
+
+    # Obtenir le chemin complet du fichier
+    a = os.path.dirname(item)
+    
+    # Split le chemin complet du fichier par '/'
+    a = a.split('/')
+    
+    # Retourner le dernier élément de la liste, qui est le nom du répertoire
+    a = a[-1]
+    
+    return a
+
+
+
+
--- main.py
+++ main.py
@@ -1,29 +1,43 @@
 """Module providing to test some functions"""
 
+import os
+
 from dictionnaires.pfo_dictionnaires import printd
+
 from listes.pfo_listes import printl
-from files.pfo_files import list_files_in_repertoire
+
+from files.pfo_files import list_all_files
+from files.pfo_files import get_directory_name
+
 from photos.pfo_photos import extract_some_exif_data
 from photos.pfo_photos import change_date
 from photos.pfo_photos import change_copyright
 
-chemin = '/Users/pierre/Desktop/images'
+chemin = '/Users/pierre/Desktop/images/divers'
 
 filename = 'PXL_20240720_123424094.MP.jpg'
 
+# change_date(chemin, filename, '1998:01:01 00:00:01')
+# change_copyright(chemin, filename, 'ernest')
 
-change_date(chemin, filename, '1998:01:01 00:00:01')
-change_copyright(chemin, filename, 'ernest')
+# selection = [
+#     'datetime_original',
+#     'copyright'
+#     ]
 
-selection = [
-    'datetime_original',
-    'copyright'
-    ]
+# data = extract_some_exif_data(chemin, filename, selection)
 
-data = extract_some_exif_data(chemin, filename, selection)
-
-printd(data)
+# printd(data)
 
 
 # a = list_files_in_repertoire(chemin)
 # printl(a)
+
+liste = list_all_files(chemin)
+
+for item in liste:
+    print(item)
+    print(get_directory_name(item))
+    
+
+
