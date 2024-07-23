ade13faf66fcd85cfa8d0cd97392f6c7ba5d0519
pfoisse <49710916+pfoisse@users.noreply.github.com>
2024-07-23 10:05:23 +0200

    creation d un dataframe avec le contenu des repertoires et sous repertoires

--- files/pfo_files.py
+++ files/pfo_files.py
@@ -1,6 +1,7 @@
 """Module providing some functions to work with files"""
 
 import os
+import pandas as pd
 
 # ! --------------------------------------------------------------------------------------
 def list_files_in_directory(folder_path):
@@ -31,7 +32,8 @@ def list_all_files(folder_path):
     """
     Lister le contenu d un répertoire y compris dans les sous-répertoires
     Cette fonction liste les fichiers présents dans un répertoire spécifique 
-    ainsi que dans tous les sous-répertoires.
+    ainsi que dans tous les sous-répertoires. 
+    Cette fonction ne liste pas les fichés cachés (ceux qui commencent par un point).
 
     Args:
         folder_path (chain): chemin vers le répertoire
@@ -53,10 +55,11 @@ def list_all_files(folder_path):
     for subdir, _, files in os.walk(rootdir):
         # Lister les fichiers dans le répertoire
         for file in files:
-            # Ajouter le chemin complet du fichier à la liste
-            a = os.path.join(subdir, file)
-            # Ajouter le chemin à la liste
-            liste.append(a)
+            if not file.startswith('.'):
+                # Ajouter le chemin complet du fichier à la liste
+                a = os.path.join(subdir, file)
+                # Ajouter le chemin à la liste
+                liste.append(a)
     
     liste = sorted(liste)
 
@@ -65,7 +68,6 @@ def list_all_files(folder_path):
 # ! --------------------------------------------------------------------------------------
 def get_directory_name(folder_path):
     """
-    Donner le nom du répertoire qui contient un fichier
     Cette fonction donne le nom du répertoire qui contient un fichier spécifique.
     Dans ce code, la fonction get_directory_name utilise la fonction os.path.dirname
     pour obtenir le nom du répertoire qui contient le fichier. 
@@ -74,11 +76,12 @@ def get_directory_name(folder_path):
     de la liste, qui est le nom du répertoire.
 
     Args:
-        file (chain): nom du fichier
-        ex : ''
+        folder_path (chain): chemin vers le nom du fichier
+        ex : '/Users/pierre/Desktop/images/divers/2019-02-12/IMAG0045.JPG'
 
     Returns:
-        (chain) nom du répertoire 
+        (chain) : le nom du répertoire qui contient le fichier
+        ex : 2019-02-12
 
     Example:
         
@@ -96,5 +99,76 @@ def get_directory_name(folder_path):
     return a
 
 
+# ! --------------------------------------------------------------------------------------
+def get_file_name(folder_path):
+    """
+    La fonction get_file_name prend en paramètre un chemin complet vers un fichier 
+    spécifique. Elle utilise la méthode split pour séparer le chemin complet 
+    du fichier par '/' et retourne le dernier élément de la liste, 
+    qui est le nom du fichier. 
+    Cette fonction est utile pour extraire le nom du fichier 
+    à partir de son chemin complet.
+
+    Args:
+        folder_path (chain): chemin vers le nom du fichier
+        ex : '/Users/pierre/Desktop/images/divers/2019-02-12/IMAG0045.JPG'
+
+    Returns:
+        (chain) : le nom du fichier
+        ex : IMAG0045.JPG
+
+    Example:
+        
+    """
+    
+    # Split le chemin complet du fichier par '/'
+    a = folder_path.split('/')
+    
+    # Retourner le dernier élément de la liste, qui est le nom du fichier
+    a = a[-1]
+    
+    return a
 
 
+# ! --------------------------------------------------------------------------------------
+def get_all_files_content_in_a_dataframe(folder_path):
+    """
+    La fonction get_all_files_content_in_a_dataframe prend en entrée un chemin d'accès 
+    à un dossier et renvoie un DataFrame contenant le contenu du dossier. 
+    Elle liste d'abord tous les fichiers dans le dossier 
+    et dans tous les sous-dossiers en utilisant la fonction list_all_files. 
+    Ensuite, elle crée une liste de dictionnaires pour stocker les informations 
+    des fichiers, y compris le chemin du fichier, le nom du fichier et le nom du dossier. 
+    Enfin, elle convertit la liste de dictionnaires en un DataFrame pandas.
+
+    Args:
+        folder_path (chain): chemin vers le nom du fichier
+        ex : '/Users/pierre/Desktop/images/divers/2019-02-12/IMAG0045.JPG'
+
+    Returns:
+        (chain) : le nom du fichier
+        ex : IMAG0045.JPG
+
+    Example:
+        
+    """
+
+    liste = list_all_files(folder_path)
+
+    # Créer une liste de dictionnaires pour stocker les données
+    data_list = []
+
+    for chemin in liste:
+        directory_name = get_directory_name(chemin)
+        file_name = get_file_name(chemin)
+        data_dict = {
+            'chemin': chemin,
+            'nom du fichier': file_name,
+            'nom du répertoire': directory_name
+        }
+        data_list.append(data_dict)
+
+    # Convertir la liste de dictionnaires en un DataFrame pandas
+    df = pd.DataFrame(data_list)
+
+    return(df)
--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-"""Module providing to test some functions"""
-
-
+"""Module provided to test some functions"""
 
 from dictionnaires.pfo_dictionnaires import printd
 
+import pandas as pd
+
 from listes.pfo_listes import printl
 
-from files.pfo_files import list_all_files
-from files.pfo_files import get_directory_name
+from files.pfo_files import get_all_files_content_in_a_dataframe
 
 from photos.pfo_photos import extract_some_exif_data
 from photos.pfo_photos import change_date
@@ -15,7 +14,11 @@ from photos.pfo_photos import change_copyright
 
 chemin = '/Users/pierre/Desktop/images/divers'
 
-filename = 'PXL_20240720_123424094.MP.jpg'
+print(get_all_files_content_in_a_dataframe(chemin))
+
+
+
+
 
 # change_date(chemin, filename, '1998:01:01 00:00:01')
 # change_copyright(chemin, filename, 'ernest')
@@ -33,11 +36,5 @@ filename = 'PXL_20240720_123424094.MP.jpg'
 # a = list_files_in_repertoire(chemin)
 # printl(a)
 
-liste = list_all_files(chemin)
-
-for item in liste:
-    print(item)
-    print(get_directory_name(item))
-    
 
 
